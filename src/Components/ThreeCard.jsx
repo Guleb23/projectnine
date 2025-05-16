7f22920e894d0979e90d4af73e16a3eefd24e677
Guleb23 <112973753+Guleb23@users.noreply.github.com>
2025-05-16 03:57:41 +0300

    ыы

--- src/Components/ThreeCard.jsx
+++ src/Components/ThreeCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ThreeCard = ({ img, mainText, subText, customStyles, ref }) => {
     return (
-        <div ref={ref} className={`z-30  backdrop-blur-xl bg-black/20 flex grow-0 shrink-0 flex-col gap-6 sm:w-[294px] w-[235px] ${customStyles || ""}`}>
+        <div ref={ref} className={`z-30  backdrop-blur-xl  flex grow-0 shrink-0 flex-col gap-6 sm:w-[294px] w-[235px] ${customStyles || ""}`}>
             <img className='select-none pointer-events-none  card-img ' width={294} height={294} src={img} alt='img' />
             <div className='flex flex-col gap-2'>
                 <p className='mono sm:text-[23px] text-[15px] font-bold gradient-text-green card-title'>{mainText}</p>
