f9172dcdb097e4cd0d54a02f5daa76cfc1bee8e7
Guleb23 <112973753+Guleb23@users.noreply.github.com>
2025-05-21 05:21:42 +0300

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-import { Suspense, lazy } from 'react'
 
+
+import HeroSection from "./Sections/HeroSection"
+import NewSecondSection from "./Sections/NewSecond/NewSecondSection"
+import SecondSection from "./Sections/SecondSection"
+import ThreeScetion from "./Sections/ThreeScetion"
+import FourSection from "./Sections/FourSection/FourSection"
+import SevenSection from "./Sections/SevenSection/SevenSection"
+import NineSection from "./Sections/NineSection/NineSection"
 
 function App() {
   return (
