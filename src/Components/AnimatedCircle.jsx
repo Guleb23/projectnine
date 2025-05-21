9cfdbac5579727c29032abbfd051336b91c4495f
Guleb23 <112973753+Guleb23@users.noreply.github.com>
2025-05-21 05:23:29 +0300

    asd

--- src/Components/AnimatedCircle.jsx
+++ src/Components/AnimatedCircle.jsx
@@ -1,42 +1,19 @@
-import { useEffect, useState, lazy, Suspense } from 'react';
+import Lottie from 'lottie-react';
+import animationData from '/public/Ball/main.json'; // путь к Lottie JSON-файлу
+import { useEffect } from 'react';
 
-const Lottie = lazy(() => import('lottie-react'));
 
 const AnimatedCircle = ({ lottieRef, customStyle, width, height, id }) => {
-    const [animationData, setAnimationData] = useState(null);
-    const [shouldRender, setShouldRender] = useState(false);
-
-    useEffect(() => {
-        // Задержка для сглаживания появления
-        const timeout = setTimeout(() => {
-            setShouldRender(true);
-
-            // Асинхронная загрузка JSON файла
-            import('/public/Ball/main.json')
-                .then((data) => setAnimationData(data.default))
-                .catch((err) => console.error("Failed to load animation JSON", err));
-        }, 10);
-
-        return () => clearTimeout(timeout);
-    }, []);
 
     return (
         <div
             id={id}
             ref={lottieRef}
             className={`absolute pt-[2%] select-none pointer-events-none ${customStyle || ""}`}
-            style={{ width: `${width}px`, height: `${height}px`, willChange: 'transform, opacity' }}
+
+            style={{ width: `${width}px`, height: `${height}px` }}
         >
-            {shouldRender && animationData && (
-                <Suspense fallback={<div style={{ width: '100%', height: '100%' }} />}>
-                    <Lottie
-                        animationData={animationData}
-                        loop
-                        autoplay
-                        style={{ width: '100%', height: '100%' }}
-                    />
-                </Suspense>
-            )}
+            <Lottie animationData={animationData} loop autoplay style={{ width: '100%', height: '100%' }} />
         </div>
     );
 };
