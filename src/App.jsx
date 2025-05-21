2e79b8a8cfa919f8a5258b63575ac8153395bee6
Guleb23 <112973753+Guleb23@users.noreply.github.com>
2025-05-21 05:20:47 +0300

    nazad

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,18 @@
 import { Suspense, lazy } from 'react'
-import Loader from './Components/Loader'
 
-const HeroSection = lazy(() => import("./Sections/HeroSection"))
-const NewSecondSection = lazy(() => import("./Sections/NewSecond/NewSecondSection"))
-const SecondSection = lazy(() => import("./Sections/SecondSection"))
-const ThreeScetion = lazy(() => import("./Sections/ThreeScetion"))
-const FourSection = lazy(() => import("./Sections/FourSection/FourSection"))
-const SevenSection = lazy(() => import("./Sections/SevenSection/SevenSection"))
-const NineSection = lazy(() => import("./Sections/NineSection/NineSection"))
 
 function App() {
   return (
     <main>
-      <Suspense fallback={<Loader />}>
-        <HeroSection />
-        <NewSecondSection />
-        <SecondSection />
-        <ThreeScetion />
-        <FourSection />
-        <SevenSection />
-        <NineSection />
-      </Suspense>
+
+      <HeroSection />
+      <NewSecondSection />
+      <SecondSection />
+      <ThreeScetion />
+      <FourSection />
+      <SevenSection />
+      <NineSection />
+
     </main>
   )
 }
